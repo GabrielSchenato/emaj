eb901010bd3bd07863209f6120fffad8ccd0be86
Gabriel Schenato <gabrielschenato152@hotmail.com>
2019-03-04 18:45:30 -0300

    Ajuste para exibir version da aplicação

--- config/app.php
+++ config/app.php
@@ -13,7 +13,20 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'EMAJ'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Version
+    |--------------------------------------------------------------------------
+    |
+    | This value is the version of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    |
+    */
+
+    'version' => '1.0.0',
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <script src="https://cdn.bootcss.com/echarts/4.0.4/echarts-en.min.js"></script>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="shortcut icon" href="/favicon.ico" />
-        <title>{{ env('APP_NAME') }} - (Versão:. {{ env('APP_VERSION') }})</title>
+        <title>{{ env('APP_NAME') }} - (Versão:. {{ config('app.version') }})</title>
 
     </head>
     <body>
