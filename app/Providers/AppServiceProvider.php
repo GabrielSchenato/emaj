0dee13700dea2f93728680fa9cbd2469105a89e1
Gabriel Schenato <gabrielschenato152@hotmail.com>
2019-03-13 08:35:39 -0300

    Configurações para https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
