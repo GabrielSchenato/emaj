ad101110b8250dbef8ca261fd8584db22cc6ff1e
Gabriel Schenato <gabrielschenato152@hotmail.com>
2019-04-05 20:23:47 -0300

    Adicionado página em construção

--- resources/views/institucional/contato.blade.php
+++ resources/views/institucional/contato.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
+<section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row no-gutters slider-text align-items-end justify-content-center">
+            <div class="col-md-9 ftco-animate pb-5 text-center">
+                <h1 class="mb-3 bread">Contato</h1>
+                <p class="breadcrumbs">
+                    <span class="mr-2">
+                        <a href="{{ route('institucional.home') }}">
+                            Início 
+                            <i class="ion-ios-arrow-forward"></i>
+                        </a>
+                    </span>
+                    <span>
+                        Contato 
+                        <i class="ion-ios-arrow-forward"></i>
+                    </span>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
 
+<section class="ftco-section services-section bg-light">
+    <div class="container mt-0">
+        <div class="row justify-content-center mb-5 pb-3">
+            <div class="col-md-7 text-center heading-section ftco-animate">
+                <h2 class="mb-4">Página em construção... Aguarde</h2>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
