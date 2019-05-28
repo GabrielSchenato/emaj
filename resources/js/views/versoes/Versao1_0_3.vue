f9e2623653423dda2392d00fd8baa84b7038a3c5
Gabriel Schenato <gabrielschenato152@hotmail.com>
2019-05-28 20:01:57 -0300

    Adaptação (Movimento) Aplicada melhoria na impressão da 'Ficha de Triagem', aumentando o campo 'Local Trabalho'

---
+++ resources/js/views/versoes/Versao1_0_3.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card class="card--flex-toolbar">
+        <v-toolbar card prominent>
+            <v-toolbar-title class="body-6 black--text">v1.0.3</v-toolbar-title>
+            <v-toolbar-title class="body-1 grey--text"><small>28 de Maio de 2019</small></v-toolbar-title>                      
+            <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-divider></v-divider>
+        <v-card-text>
+
+            <h3 class="font-weight-thin font-italic">Portal</h3>
+            <v-divider></v-divider>
+            
+            <v-list-tile>
+                <v-list-tile-action>
+                    <v-icon>fiber_manual_record</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    Adaptação (Movimento) Aplicada melhoria na impressão da "Ficha de Triagem", aumentando o campo "Local Trabalho"
+                </v-list-tile-content>
+            </v-list-tile> 
+           
+            <br>
+
+            <h3 class="font-weight-thin font-italic">Institucional</h3>
+            <v-divider></v-divider>         
+
+        </v-card-text>
+    </v-card>    
+</template>
+
+<script>
+    export default {
+        name: "versao1_0_3"
+    };
+</script>
