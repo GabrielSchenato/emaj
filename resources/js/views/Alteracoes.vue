38562bf6433f69ed49158e6bccd564fb5b417643
Gabriel Schenato <gabrielschenato152@hotmail.com>
2019-06-08 16:49:18 -0300

    Melhorias diversas

--- resources/js/views/Alteracoes.vue
+++ resources/js/views/Alteracoes.vue
@@ -1,6 +1,8 @@
 <template>
     <v-layout row pb-2>
         <v-flex xs10 offset-xs1>
+            <versao1_0_4></versao1_0_4>
+            </br>
             <versao1_0_3></versao1_0_3>
             </br>
             <versao1_0_2></versao1_0_2>
@@ -17,12 +19,14 @@
     import Versao1_0_1 from "@/views/versoes/Versao1_0_1.vue";
     import Versao1_0_2 from "@/views/versoes/Versao1_0_2.vue";
     import Versao1_0_3 from "@/views/versoes/Versao1_0_3.vue";
+    import Versao1_0_4 from "@/views/versoes/Versao1_0_4.vue";
     export default {
         components: {
             Versao1_0_0,
             Versao1_0_1,
             Versao1_0_2,
             Versao1_0_3,
+            Versao1_0_4,
         }
     };
 </script>
