48ad5e63848d5ef2c8a45011127f975957e18dae
Gabriel Schenato <gabrielschenato152@hotmail.com>
2019-08-20 19:22:47 -0300

    Ajustes

---
+++ database/migrations/2019_08_18_201310_add_column_protocolo_id_avaliacoes.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnProtocoloIdAvaliacoes extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('avaliacoes', function (Blueprint $table) {
+            $table->unsignedBigInteger('protocolo_id')->after('aluno_id')->nullable(true);
+            $table->foreign('protocolo_id')->references('id')->on('protocolos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropColumn('protocolo_id');
+    }
+
+}
