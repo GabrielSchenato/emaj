e66c91f9d451fc750a4541c2a35a235da11d4d29
Gabriel Schenato <gabrielschenato152@hotmail.com>
2019-09-28 19:19:52 -0300

    Evolução (Relatório): Criado novo relatório "Clientes e Protocolos", onde o mesmo traz os protocolos ativos e inativos que um cliente possui

--- resources/js/components/relatorio/forms/RelatorioClientesProtocolosForm.vue
+++ resources/js/components/relatorio/forms/RelatorioClientesProtocolosForm.vue
@@ -78,7 +78,7 @@
 </template>
 <script>
     export default {
-        name: "relatorio-professores-alunos-form",
+        name: "relatorio-clientes-protocolos-form",
         props: {
             value: {
                 type: [Object]
